e8178357665afaf8d060ed55624a962f79e1deec
wangsop <sophia.riverfront@gmail.com>
2025-01-18 19:23:34 -0500

    Pause menu
    So this push includes the change to the dialogue manager that I asked about in discord (advancing dialogue now requires that you hit N key), but the pause menu works on the bright side? If you want, you can delete the DialogueManager.cs change, or just send it back, but I really need to push this while my Unity/GitHub Desktop are being cooperative.

---
+++ Assets/Scenes/Sophia Wang/PauseScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript1 : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
--- Assets/Scripts/Manager/DialogueManager.cs
+++ Assets/Scripts/Manager/DialogueManager.cs
@@ -52,10 +52,14 @@ public class DialogueManager : MonoBehaviour
     {
         if (GameManager.Instance.state == State.Dialogue)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.N))
             {
                 AdvanceDialogue();
             }
+            /*if (Input.GetMouseButtonDown(0))
+            {
+                AdvanceDialogue();
+            }*/
         }
     }
 
