2237a168887415df71532e7ce7c85024020773eb
Sahil Chawla <sahilchawla2312@gmail.com>
2025-01-22 21:44:06 -0500

    feat: integrated currency data population

--- Assets/Scripts/Manager/RecipeShopManager.cs
+++ Assets/Scripts/Manager/RecipeShopManager.cs
@@ -1,16 +1,28 @@
 using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RecipeShopManager : MonoBehaviour
 {
+    
+    public GameObject recipePrefab;
+    
+    //Recipes to populate the shop with
+    [SerializeField] private List<Recipe> recipes;
+    
+    //UI references
     [SerializeField] private GameObject recipeShop;
-
+    [SerializeField] private TextMeshProUGUI currencyText;
     [SerializeField] private Button recipeShopOpenButton;
     [SerializeField] private Button recipeShopReturnButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Set shop data including recipes and currency
+        PopulateShop();
+        
         //Initially hide and show relevant gameobjects
         recipeShopOpenButton.gameObject.SetActive(true);
         recipeShopReturnButton.gameObject.SetActive(false);
@@ -20,9 +32,23 @@ public class RecipeShopManager : MonoBehaviour
         recipeShopOpenButton.onClick.AddListener(ShowRecipeShop);
         recipeShopReturnButton.onClick.AddListener(HideRecipeShop);
     }
-    
 
-    public void ShowRecipeShop()
+    private void PopulateShop()
+    {
+        // Update currency text
+        SetCurrencyData();
+ 
+        //Iterate through all recipes
+        //Instaniate and populate data in recipe prefab
+        //Add to shop content
+    }
+
+    private void SetCurrencyData()
+    {
+        currencyText.text = "Currency: $" + GameManager.Instance.GetCurrency().ToString();
+    }
+    
+    private void ShowRecipeShop()
     {
         recipeShop.SetActive(true);
         
@@ -31,7 +57,7 @@ public class RecipeShopManager : MonoBehaviour
         recipeShopReturnButton.gameObject.SetActive(true);
     }
 
-    public void HideRecipeShop()
+    private void HideRecipeShop()
     {
         recipeShop.SetActive(false);
         
