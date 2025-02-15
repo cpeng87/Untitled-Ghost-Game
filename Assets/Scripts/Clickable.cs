6f1ecc9970aef1eec5d863dd6d6ed135dd328047
cpeng87 <106842816+cpeng87@users.noreply.github.com>
2025-02-15 15:12:29 -0500

    sweet freedom

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class Clickable : MonoBehaviour
 {
-    void Update()
+    public void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.gameObject == gameObject)
