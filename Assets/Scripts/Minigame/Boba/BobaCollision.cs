02b6c9aa305b759f116eeef357eacd48ccfc21f0
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2025-03-12 20:03:23 -0400

    Boba Update
    - Made a Boba Spawner with parameters for number of boba spawned, height above object spawned at, and cooldown for spawn
    - Added Boba, Pot, and Strainer Prefabs / Assets
    - Made a Boba Script to control the pot movement

---
+++ Assets/Scripts/Minigame/Boba/BobaCollision.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BobaCollision : MonoBehaviour
+{
+    private bool isTriggered = false; //ensures boba doesn't infinitely update the counter 
+
+    private void OnTriggerEnter(Collider collider)
+    {
+
+        GameObject potPrefab = GameObject.Find("Pot");
+        GameObject boundsPrefab = GameObject.Find("StrainerAndBounds/Bounds");
+        Debug.Log(collider.gameObject.name);
+
+        if (collider.gameObject.name.Equals(boundsPrefab.name) && !isTriggered) //if the boba hits the bounds collider...
+        {
+            isTriggered = true;
+            //Debug.Log("BobaHitStrainer - " + collider.gameObject.name);
+            var script = potPrefab.GetComponent<BobaPotController>();
+            script.OnBobaAdd(); // update boba counter
+        } 
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        GameObject potPrefab = GameObject.Find("Pot");
+        GameObject boundsPrefab = GameObject.Find("StrainerAndBounds/Bounds");
+        if (collider.gameObject.name.Equals(boundsPrefab.name)) //if the boba leaves the bounds collider...
+        {
+            if (transform.position.y > boundsPrefab.transform.position.y) { //only if the boba is above the collider!
+                isTriggered = false;
+                //Debug.Log("BobaLeftStrainer");
+                var script = potPrefab.GetComponent<BobaPotController>();
+                script.OnBobaRemove(); //update boba counter
+            }
+        }
+    }
+}
