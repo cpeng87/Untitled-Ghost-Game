969ebdb84ec93e404eec2dee6d4cf7385bc7373f
User <alcp6201@gmail.com>
2026-09-26 13:49:28 -0400

    saving credits progress, added reaps waving

--- Assets/Scripts/UI/CreditsRoll.cs
+++ Assets/Scripts/UI/CreditsRoll.cs
@@ -7,6 +7,7 @@ public class CreditsRoll : MonoBehaviour
     [SerializeField] private GameObject creditsNamePanel;
     [SerializeField] private float speed;
     [SerializeField] private List<GameObject> spinnyGhosts = new List<GameObject>();
+    // [SerializeField] private GameObject spinnyGhostHolder;
     private float time;
 
     [SerializeField] private GameObject lawyer;
@@ -22,6 +23,9 @@ public class CreditsRoll : MonoBehaviour
     [SerializeField] private GameObject soldier;
     [SerializeField] private GameObject doctor;
     bool hasPeeked = false;
+
+    [SerializeField] private GameObject reaper;
+    [SerializeField] private GameObject reaperHand;
 
 
     //event flags
@@ -40,13 +44,7 @@ public class CreditsRoll : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
-        creditsNamePanel.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
-
-        if (creditsNamePanel.transform.position.y >= 67f)
-        {
-            //transition to title screen
-            MoveToTitle();
-        }
+        creditsNamePanel.transform.position += new Vector3(0f, speed * 0.85 * Time.deltaTime, 0f);
 
         if (!lawyerDogDone)
         {
@@ -63,6 +61,10 @@ public class CreditsRoll : MonoBehaviour
         else if (!spinnyDone)
         {
             SpinnyGhostDown();
+        }
+        else
+        {
+            ReaperWave();
         }
     }
 
@@ -82,9 +84,9 @@ public class CreditsRoll : MonoBehaviour
     {
         foreach (GameObject ghost in spinnyGhosts)
         {
-            ghost.transform.position -= new Vector3(0f, speed * 0.8f * Time.deltaTime, 0f);
+            ghost.transform.position -= new Vector3(0f, speed * Time.deltaTime * 1.05f, 0f);
             ghost.transform.Rotate(0f, speed * 100f * Time.deltaTime, 0f);
-            if (ghost.transform.position.y < -21)
+            if (ghost.transform.position.y < -48.1f)
             {
                 spinnyDone = true;
             }
@@ -94,11 +96,11 @@ public class CreditsRoll : MonoBehaviour
     //holy hardcode
     private void LawyerAndDog()
     {
-        if (lawyer.transform.position.x > 5.73f && lawyerHasFacedPlayer == false) 
+        if (lawyer.transform.position.x > 6.73f && lawyerHasFacedPlayer == false) 
         {
             lawyer.transform.position -= new Vector3(speed * 2f * Time.deltaTime, 0, 0);
         }
-        else if (dog.transform.position.x < 2.77f)
+        else if (dog.transform.position.x < 3.77f)
         {
             dog.transform.position += new Vector3(4f * speed * Time.deltaTime, 0, 0);
         }
@@ -129,29 +131,29 @@ public class CreditsRoll : MonoBehaviour
 
     private void WeebAndOffice()
     {
-        if (officeWorker.transform.position.x <= -4.44f)
+        if (officeWorker.transform.position.x >= -4.09f)
         {
-            weeb.transform.position += new Vector3(speed * 2f * Time.deltaTime, 0, 0);
-            officeWorker.transform.position += new Vector3(speed * 2f * Time.deltaTime, 0, 0);
+            weeb.transform.position -= new Vector3(speed * 2f * Time.deltaTime, 0, 0);
+            officeWorker.transform.position -= new Vector3(speed * 2f * Time.deltaTime, 0, 0);
         }
         else if (Quaternion.Angle(weeb.transform.rotation, Quaternion.Euler(0, 90, 0)) > 0.1f && weebHasFacedPlayer == false)
         {
             weeb.transform.rotation = Quaternion.Lerp(weeb.transform.rotation, Quaternion.Euler(0, 90, 0), speed * 2f * Time.deltaTime);
             officeWorker.transform.rotation = Quaternion.Lerp(officeWorker.transform.rotation, Quaternion.Euler(0, 90, 0), speed * 2f * Time.deltaTime);
         }
-        else if (Quaternion.Angle(weeb.transform.rotation, Quaternion.Euler(0, 0, 0)) > 1f)
+        else if (Quaternion.Angle(weeb.transform.rotation, Quaternion.Euler(0, 180, 0)) > 1f)
         {
             weebHasFacedPlayer = true;
-            weeb.transform.rotation = Quaternion.Lerp(weeb.transform.rotation, Quaternion.Euler(0, 0, 0), speed * 2f * Time.deltaTime);
-            officeWorker.transform.rotation = Quaternion.Lerp(officeWorker.transform.rotation, Quaternion.Euler(0, 0, 0), speed * 2f * Time.deltaTime);
+            weeb.transform.rotation = Quaternion.Lerp(weeb.transform.rotation, Quaternion.Euler(0, 180, 0), speed * 2f * Time.deltaTime);
+            officeWorker.transform.rotation = Quaternion.Lerp(officeWorker.transform.rotation, Quaternion.Euler(0, 180, 0), speed * 2f * Time.deltaTime);
         }
         else
         {
-            weeb.transform.position += new Vector3(speed * 2f * Time.deltaTime, 0, 0);
-            officeWorker.transform.position += new Vector3(speed * 2f * Time.deltaTime, 0, 0);
+            weeb.transform.position -= new Vector3(speed * 2f * Time.deltaTime, 0, 0);
+            officeWorker.transform.position -= new Vector3(speed * 2f * Time.deltaTime, 0, 0);
         }
 
-        if (weeb.transform.position.x >= 11.6f)
+        if (officeWorker.transform.position.x <= -10.74f)
         {
             weebOfficeDone = true;
         }
@@ -163,19 +165,19 @@ public class CreditsRoll : MonoBehaviour
         {
             if (musician.transform.position.y < -4.16f)
             {
-                musician.transform.position += new Vector3(0f, speed * Time.deltaTime * 2f, 0f);
+                musician.transform.position += new Vector3(0f, speed * Time.deltaTime * 3f, 0f);
             }
             else if (doctor.transform.position.y > 6.24f)
             {
-                doctor.transform.position -= new Vector3(0f, speed * Time.deltaTime * 2f, 0f);
+                doctor.transform.position -= new Vector3(0f, speed * Time.deltaTime * 3f, 0f);
             }
-            else if (jock.transform.position.x < -9.8f)
+            else if (jock.transform.position.x < -9.2f)
             {
-                jock.transform.position += new Vector3(speed * Time.deltaTime * 2f, 0f, 0f);
+                jock.transform.position += new Vector3(speed * Time.deltaTime * 3f, 0f, 0f);
             }
-            else if (soldier.transform.position.x > 9.6f)
+            else if (soldier.transform.position.x > 9f)
             {
-                soldier.transform.position -= new Vector3(speed * Time.deltaTime * 2f, 0f, 0f);
+                soldier.transform.position -= new Vector3(speed * Time.deltaTime * 3f, 0f, 0f);
             }
             else
             {
@@ -186,24 +188,34 @@ public class CreditsRoll : MonoBehaviour
         {
             if (musician.transform.position.y > -6.67f)
             {
-                musician.transform.position -= new Vector3(0f, speed * Time.deltaTime * 2f, 0f);
+                musician.transform.position -= new Vector3(0f, speed * Time.deltaTime * 3f, 0f);
             }
             else if (doctor.transform.position.y < 7.81f)
             {
-                doctor.transform.position += new Vector3(0f, speed * Time.deltaTime * 2f, 0f);
+                doctor.transform.position += new Vector3(0f, speed * Time.deltaTime * 3f, 0f);
             }
             else if (jock.transform.position.x > -12f)
             {
-                jock.transform.position -= new Vector3(speed * Time.deltaTime * 2f, 0f, 0f);
+                jock.transform.position -= new Vector3(speed * Time.deltaTime * 3f, 0f, 0f);
             }
             else if (soldier.transform.position.x < 11.67f)
             {
-                soldier.transform.position += new Vector3(speed * Time.deltaTime * 2f, 0f, 0f);
+                soldier.transform.position += new Vector3(speed * Time.deltaTime * 3f, 0f, 0f);
             }
             else
             {
                 peekDone = true;
             }
+        }
+    }
+
+    private void ReaperWave()
+    {
+        reaper.transform.position += new Vector3(0f, speed * 0.85f * Time.deltaTime, 0f);
+
+        if (reaper.transform.position.y >= 13.76f)
+        {
+            MoveToTitle();
         }
     }
 }
